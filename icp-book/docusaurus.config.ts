8b07ab4ce4a19dde403dedf2f4886edb53c21946
Muhammad Fasih <programmer.fasih.18@gmail.com>
2025-12-01 22:28:17 +0500

    Update docusaurus.config.ts

--- icp-book/docusaurus.config.ts
+++ icp-book/docusaurus.config.ts
@@ -119,7 +119,7 @@ const config: Config = {
           items: [
             {
               label: 'Introduction',
-              to: '/docs/introduction',
+              to: '/docs/',
             },
             {
               label: 'Ideology of Pakistan',
